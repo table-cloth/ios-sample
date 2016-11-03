b023217973c1fa017a3f2b08528446696a587849
ShuntaNomura <nomura@ShuntaNomuranoMacBook-Pro.local>
2016-11-03 13:50:18 +0700

    control drag text field from Main.storyboard -> ViewController.swift

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
